7b6305d63d993ae4a5bc6440282288aa07b007d9
Nikita Ustiuzhanin <zaebumba@gmail.com>
2023-09-27 08:42:06 +0700

    Add v1

--- core/urls.py
+++ core/urls.py
@@ -31,7 +31,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('api/', include('mailing.urls')),
+    path('api/v1/', include('mailing.urls')),
     path('admin/', admin.site.urls),
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('', include('django_prometheus.urls')),
